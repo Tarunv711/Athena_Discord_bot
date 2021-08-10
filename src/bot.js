cf1fe9d6dcd0c5d0991d315615fa6c2eeea67df7
Tarun Venkatasamy <tarunv711@gmail.com>
2021-08-10 15:05:26 -0400

    Third commit, doesn't work but not far from working

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,12 @@
 ﻿require('dotenv').config();
 
+const Discord = require('discord.js');
+
 const { Client } = require('discord.js');
 
 const client = new Client();
+
+//const channel = client.channels.cache.get(`${message.channel.id}`);
 
 client.on('ready', () =>{
     console.log(`${client.user.tag} has logged in. `);
@@ -11,8 +15,79 @@ client.on('ready', () =>{
 client.on('message', (message) =>{
     console.log(`[${message.author.tag}]: ${message.content}`);
     if(message.content == 'hello'){
-        message.reply('hello there');
+        const exampleEmbed = new Discord.MessageEmbed()
+        .setTitle('Some title')
+        .setURL('https://www.youtube.com/watch?v=O7ulUVts3wI')
+        //.setImage('https://qa--athenaviewer.netlify.app/prompts/1374a555-75dd-40ff-929f-7727ddf85218')
+        .addFields({
+            video: {
+                url: 'https://www.youtube.com/watch?v=O7ulUVts3wI',
+                proxyURL: 'https://www.youtube.com/watch?v=O7ulUVts3wI',
+                height: 500,
+                width: 500,
+            }
+            }
+        )
+        .setTimestamp();
+    
+        
+        message.channel.send(exampleEmbed);
     }
 } );
 
+client.on('message', (message) =>{
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if(message.content == 'vid'){
+        const emb = {
+            type: `video`,
+            title: 'Some title',
+            url: 'https://qa--athenaviewer.netlify.app/prompts/1374a555-75dd-40ff-929f-7727ddf85218',
+
+            
+            image: {
+                url: 'https://www.youtube.com/watch?v=O7ulUVts3wI',
+                proxyURL: 'https://www.youtube.com/watch?v=O7ulUVts3wI',
+                height: 500,
+                width: 500,
+            },
+                
+            //timestamp: new Date()
+            
+
+        }
+        message.channel.send({embed: emb});
+    }
+        });
+
+        let gif = new Discord.MessageEmbed() 
+        .setColor(0x000000)
+        .setDescription(` `)
+        .attachFiles(["https://media.giphy.com/media/USQldFTYp0CFd2wmrc/giphy.gif"]);
+
+        client.on('message', (message) =>{
+            console.log(`[${message.author.tag}]: ${message.content}`);
+            //message.toLowerCase();
+            const pref = 'https://qa--athenaviewer.netlify.app/prompts/';
+            if(message.content.startsWith(pref)){
+                const user = message.author;
+                const identity = message.content.slice(pref.length).trim();
+                message.channel.send(gif);
+                console.log(message.id);
+                //const channel  = new ThreadChannel
+                
+                const thread = message.channel.threads.create({
+                    name: `${user.id}'s Athena Prompt Thread`,
+                    autoArchiveDuration: 1440,
+                    type: 'private_thread',
+                    reason: 'Prompt Responses will be updated here',
+                });
+                thread.members.add(`${user.id}`);
+                message.channel.send(`<@${user.id}> has sent in an athena link`);
+            }
+        } );
+
+
+
+        
 client.login(process.env.DISCORDJS_BOT_TOKEN);
+
