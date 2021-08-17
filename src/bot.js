e0945a1a1866762ad2c34ac10744e3368aa63219
Tarun Venkatasamy <tarunv711@gmail.com>
2021-08-17 14:47:59 -0400

    Fifth commit, figured everythign out other than channel ID, lots of changes made

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,9 @@ const Discord = require('discord.js');
 
 const { Client } = require('discord.js');
 
-const client = new Client();
+require('discord-reply');
+
+const client = new Discord.Client();
 
 const channel = client.channels.cache.get(`864900707392159747`);
 
@@ -15,30 +17,30 @@ client.on('ready', () =>{
 
 
 
-        let gif = new Discord.MessageEmbed() 
-        .setColor(0x000000)
-        .setDescription(` `)
-        .attachFiles(["https://media.giphy.com/media/USQldFTYp0CFd2wmrc/giphy.gif"]);
+        
 
         client.on('message', (message) =>{
             console.log(`[${message.author.tag}]: ${message.content}`);
-            //message.toLowerCase();
+
             const pref = 'https://qa--athenaviewer.netlify.app/prompts/';
+
             if(message.content.startsWith(pref)){
+
                 const user = message.author;
+
+                let gif = new Discord.MessageEmbed() 
+        .setColor(0x000000)
+        .setDescription(`<@${user.id}> has sent in an athena link`)
+        .attachFiles(["https://media.giphy.com/media/USQldFTYp0CFd2wmrc/giphy.gif"]);
+
                 const identity = message.content.slice(pref.length).trim();
-                message.channel.send(gif);
+
                 console.log(message.id);
-                //const channel  = new ThreadChannel
+
+                console.log(client.id);
                 
-                const thread = channel.threads.create({
-                    name: `${user.id}'s Athena Prompt Thread`,
-                    autoArchiveDuration: 1440,
-                    type: 'private_thread',
-                    reason: 'Prompt Responses will be updated here',
-                });
-                thread.members.add(`${user.id}`);
-                message.channel.send(`<@${user.id}> has sent in an athena link`);
+                message.lineReply(gif);
+
             }
         } );
 
